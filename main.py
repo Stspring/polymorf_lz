ceb07337674c7c2b82f996456b87872e0fbdaa71
Stspring <St.spring@tutamail.com>
2025-04-21 15:48:52 +0300

    ппрпрпр

---
+++ main.py
@@ -0,0 +1,11 @@
+from Fproc import Divide
+
+def main():
+
+    file_path = "var1.csv"
+    processor = Divide(file_path)    
+    -processor     
+    processor.split_dataset()
+
+if __name__ == "__main__":
+    main()
